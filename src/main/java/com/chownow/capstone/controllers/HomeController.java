79341fb78f8c2198143ac49730defcf121047b72
Robert De la Rosa <rdelarosa39@gmail.com>
2020-12-10 12:15:52 -0600

    first commit

---
+++ src/main/java/com/chownow/capstone/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.chownow.capstone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "index";
+    }
+}
