c52c1105d0e5ddec6e91977a22cec4485943ebe6
Robert De la Rosa <rdelarosa39@gmail.com>
2020-12-12 10:23:13 -0600

    Fix: updated models to use camelCase, created pantryingredients and recipe ingredients models and added blank repos

---
+++ src/main/java/com/chownow/capstone/repos/PantryIngredientRepository.java
@@ -0,0 +1,7 @@
+package com.chownow.capstone.repos;
+
+import com.chownow.capstone.models.PantryIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PantryIngredientRepository extends JpaRepository<PantryIngredient, Long> {
+}
