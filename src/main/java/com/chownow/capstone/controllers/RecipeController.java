c444c9b258f19bd610fd7023f4b7ab809f05295c
Michael Klanica <michael.david.klanica@gmail.com>
2020-12-14 00:28:13 -0600

    need to get user info in order to edit/create recipes-views

--- src/main/java/com/chownow/capstone/controllers/RecipeController.java
+++ src/main/java/com/chownow/capstone/controllers/RecipeController.java
@@ -3,6 +3,8 @@ package com.chownow.capstone.controllers;
 import com.chownow.capstone.models.*;
 import com.chownow.capstone.repos.RecipeRepository;
 import com.chownow.capstone.repos.UserRepository;
+//import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
