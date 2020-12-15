3d842753682b2d64e2d39b087cc6e341de4a41ef
Patrick <68563859+Patrick-Larkin@users.noreply.github.com>
2020-12-15 13:44:40 -0600

    Revert "Patrick staging"

--- src/main/java/com/chownow/capstone/controllers/HomeController.java
+++ src/main/java/com/chownow/capstone/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(){
-        return "/index";
+    public String helloWorld(){
+        return "index";
     }
 }
