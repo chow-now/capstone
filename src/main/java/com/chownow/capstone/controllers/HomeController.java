7d4f0021931725a2173c93d3d02d4a29c643b3f8
Sara <sahara.tijol@gmail.com>
2020-12-21 17:00:11 -0600

    Test #8

--- src/main/java/com/chownow/capstone/controllers/HomeController.java
+++ src/main/java/com/chownow/capstone/controllers/HomeController.java
@@ -1,50 +1,22 @@
 package com.chownow.capstone.controllers;
 
-import com.chownow.capstone.models.User;
-import com.chownow.capstone.repos.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
-//    @Autowired
-//    private UserRepository userDao;
-//
-//    @Autowired
-//    private FollowRepository followDao;
-//
-//    @Autowired
-//    private RecipeIngredientRepository recipeIngredientDao;
-//
-//    @Autowired
-//    private RecipeRepository recipeDao;
-//
-//    @Autowired
-//    private PantryIngredientRepository pantryIngredientDao;
-//
-//    @Autowired
-//    private PantryRepository pantryDao;
 
     @GetMapping("/")
     public String showHomepage() {
         return "coming-soon";
     }
 
-//    @GetMapping("/about")
-//    public String showAboutPage(Model model) {
-//        return "/aboutus";
-//    }
+    @GetMapping("/about")
+    public String showAboutPage(Model model) {
+        return "aboutus";
+    }
 
-//    @GetMapping("/home")
-//    public String showUsers(Model model) {
-//        model.addAttribute("recipes", recipeDao.findAll());
-//        model.addAttribute("users", userDao.findAll());
-//
-//        return "/index";
-//    }
 }
 
