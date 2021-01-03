47c0c7c0535bb31da9658b465b9c6e902e9c2f36
Robert De la Rosa <rdelarosa39@gmail.com>
2021-01-02 21:49:10 -0600

    Update: user profile refactored

--- src/main/java/com/chownow/capstone/controllers/UserController.java
+++ src/main/java/com/chownow/capstone/controllers/UserController.java
@@ -105,7 +105,6 @@ public class UserController {
         }
         model.addAttribute("user", user);
         model.addAttribute("isOwner",userServ.isOwner(user));
-//        model.addAttribute("suggestions",getMatches(user));
         return "users/profile";
     }
 
@@ -117,7 +116,6 @@ public class UserController {
         model.addAttribute("isFollowing", true);
         model.addAttribute("user", currentUser);
         model.addAttribute("isOwner",userServ.isOwner(currentUser));
-//        model.addAttribute("suggestions",getMatches(currentUser));
         return "users/profile";
     }
 
