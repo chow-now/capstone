455babf80d2632cb91f5d23433d9c0ad932ed864
Robert De la Rosa <rdelarosa39@gmail.com>
2021-01-08 16:43:03 -0600

    Feat: implemented navbar and links

--- src/main/java/com/chownow/capstone/controllers/HomeController.java
+++ src/main/java/com/chownow/capstone/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.chownow.capstone.controllers;
 import com.chownow.capstone.models.User;
 import com.chownow.capstone.repos.*;
 import com.chownow.capstone.services.RecipeService;
+import com.chownow.capstone.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,28 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HomeController {
     @Autowired
-    private UserRepository userDao;
-
-    @Autowired
-    private FollowRepository followDao;
-
-    @Autowired
-    private RecipeIngredientRepository recipeIngredientDao;
-
-    @Autowired
-    private RecipeRepository recipeDao;
-
-    @Autowired
-    private PantryIngredientRepository pantryIngredientDao;
-
-    @Autowired
-    private PantryRepository pantryDao;
+    private UserService userServ;
 
     @Autowired
     private RecipeService recipeServ;
 
     @GetMapping("/")
     public String showHomepage(Model model) {
+        if(userServ.isLoggedIn()){
+            return "redirect:/dashboard";
+        }
         model.addAttribute("topRecipes",recipeServ.getTopFavorites());
         return "index";
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,5 @@
 (function($) {
+   // REGISTER/LOGIN MODAL JS
     $.ajax({'url': '/register-form'}).done(function (form) {
         $('#regForm').html(form);
     });
@@ -13,4 +14,64 @@
         $("#loginModal").modal("toggle");
         $("#registerModal").modal("toggle");
     })
+
+    // NAVBAR JS
+
+    // used to shrink nav bar removing paddings and adding black background
+    $('.navTrigger').click(function () {
+        $(this).toggleClass('active');
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
+        if($('body').css('overflow') == 'hidden'){
+            $('body').css('overflow','auto');
+        }else{
+            $('body').css('overflow','hidden');
+        }
+
+    });
+    // Hide Header on on scroll down
+    let didScroll;
+    let lastScrollTop = 0;
+    let delta = 5;
+    let navbarHeight = $('.main-nav').outerHeight();
+
+    $(window).scroll(function() {
+        didScroll = true;
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    });
+
+    const hasScrolled = () => {
+        let st = $(this).scrollTop();
+        // Add bg to nav if past top else remove
+        if(st > 50){
+            $('.main-nav,.navTrigger').addClass('affix');
+            $('.main-nav').css("position", "fixed");
+
+        }else{
+            $('.main-nav,.navTrigger').removeClass('affix');
+            $('.main-nav').css("position", "relative");
+            $('main').css("margin-top", "0");
+
+        }
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta) return;
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('header').hide();
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('header').show("slow");
+                $('.main-nav, .nav-trigger').removeClass(".nav-padding");
+                $('main').css("margin-top", "-25px");
+
+            }
+        }
+        lastScrollTop = st;
+    }
+
 })(jQuery);
